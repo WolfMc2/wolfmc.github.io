ddf1b5ebc26a40a2df53f853cfab0a974724fcc9
wolfmc2 <wolfmc2017@yandex.ru>
2018-09-22 17:33:48 +0300

    initional comit

---
+++ test.js
@@ -0,0 +1,6 @@
+var e =document.querySelector("button");
+var g =document.querySelector("$("#text")");
+e.addEventListener("click", function()
+{
+	g.stile.height = (p.stile.height == "800px") ? "0px" : "800px"
+});
